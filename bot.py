2b6223f231ee9c053ec1a87537c6e05d30b02700
Juanpaulo5119 <ltpcascante5119@gmail.com>
2024-10-25 18:04:07 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,25 +6,29 @@ intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
 intents.message_content = True
 # Crear un bot en la variable cliente y transferirle los privilegios
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f'Hemos iniciado sesión como {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hola'):
-        await message.channel.send("hola!")
-    elif message.content.startswith('$adios'):
-        await message.channel.send("🙂‍↕️")
-    elif message.content.startswith('$ver'):
-        await message.channel.send("no hay nada para ver (actualmente)")
-    elif message.content.startswith("$contraseña"):
-        await message.channel.send(gen_pass(10))
-    else:
-        await message.channel.send(message.content)
+
+@bot.command
+async def hello(ctx):
+    await ctx.send("hi!")
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("🙂‍↕️")
+
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
 
 client.run("MTI5Njk4MzcwMDI4OTg4MDE1Nw.GB4kX2.J9V6GNBV1EwEZBbFJwJvSCktxn9qgwnGQzhuK4")
